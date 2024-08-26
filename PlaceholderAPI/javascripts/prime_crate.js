2dbce252e8f0bd9e746375ff43afe70b329b9844
Obecalp <99992836+obecalpjava@users.noreply.github.com>
2024-08-25 21:52:26 -0400

    Add files via upload

---
+++ PlaceholderAPI/javascripts/prime_crate.js
@@ -0,0 +1,9 @@
+var placeholder = "%ecfree_prime_left%";
+
+function tag() {
+    if(placeholder == "1") {
+        return "Next open will give";
+    } 
+    return placeholder+" opens left till";
+}
+tag();
